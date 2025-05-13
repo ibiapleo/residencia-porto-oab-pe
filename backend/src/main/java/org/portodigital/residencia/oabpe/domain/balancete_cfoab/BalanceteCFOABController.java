ccd31c6566dfa108034a086c6beac81a93d19de0
ibiapleo <leonardojose159357@gmail.com>
2025-05-13 16:07:30 -0300

    feat: implementacao da importacao de dados através de planilhas (no formato csv e xlsx).

--- backend/src/main/java/org/portodigital/residencia/oabpe/domain/balancete_cfoab/BalanceteCFOABController.java
+++ backend/src/main/java/org/portodigital/residencia/oabpe/domain/balancete_cfoab/BalanceteCFOABController.java
@@ -2,18 +2,28 @@ package org.portodigital.residencia.oabpe.domain.balancete_cfoab;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.portodigital.residencia.oabpe.domain.balancete_cfoab.dto.BalanceteCFOABRequestDTO;
 import org.portodigital.residencia.oabpe.domain.balancete_cfoab.dto.BalanceteCFOABResponseDTO;
+import org.portodigital.residencia.oabpe.domain.identidade.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/balancete-cfoab")
@@ -22,6 +32,7 @@ import org.springframework.web.bind.annotation.*;
 public class BalanceteCFOABController {
 
     private final BalanceteCFOABService balanceteCFOABService;
+    private final BalanceteCFOABImportService balanceteCFOABImportService;
 
     @Operation(
             summary = "Listar Balancetes",
@@ -110,5 +121,32 @@ public class BalanceteCFOABController {
         @Parameter(description = "Novos dados do balancete")
         @RequestBody BalanceteCFOABRequestDTO request) {
         return ResponseEntity.ok(balanceteCFOABService.update(id, request));
+    }
+
+    @Operation(
+            summary = "Faz o upload de um balancete",
+            description = "Permite que um usuário envie um arquivo",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Dados do arquivo a ser enviado",
+                    required = true,
+                    content = @Content(
+                            mediaType = MediaType.MULTIPART_FORM_DATA_VALUE
+                    )
+            )
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Balancete criado com sucesso",
+                    content = @Content(schema = @Schema(implementation = BalanceteCFOABResponseDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor", content = @Content)
+    })
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<List<BalanceteCFOABResponseDTO>> uploadFile(
+            @RequestParam("file") MultipartFile file,
+            @Parameter(hidden = true) Authentication authentication
+    ) throws IOException {
+        User user = (User) authentication.getPrincipal();
+        List<BalanceteCFOABResponseDTO> result = balanceteCFOABImportService.importFromFile(file, user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 }
--- backend/src/main/java/org/portodigital/residencia/oabpe/domain/balancete_cfoab/dto/BalanceteCFOABResponseDTO.java
+++ backend/src/main/java/org/portodigital/residencia/oabpe/domain/balancete_cfoab/dto/BalanceteCFOABResponseDTO.java
@@ -1,5 +1,6 @@
 package org.portodigital.residencia.oabpe.domain.balancete_cfoab.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +17,9 @@ public class BalanceteCFOABResponseDTO {
     private String referencia;
     private String ano;
     private String periodicidade;
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dtPrevEntr;
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dtEntr;
     private Long eficiencia;
     private String usuarioId;
