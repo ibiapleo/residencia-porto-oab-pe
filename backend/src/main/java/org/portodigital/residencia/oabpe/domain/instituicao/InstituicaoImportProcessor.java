b8300d5460fa18b8a08a785cd69793e70565aee1
Davi Mendes <davigmendes137@gmail.com>
2025-05-21 18:41:51 -0300

    feat: implementação da impotação de instituição

---
+++ backend/src/main/java/org/portodigital/residencia/oabpe/domain/instituicao/InstituicaoImportProcessor.java
@@ -0,0 +1,54 @@
+package org.portodigital.residencia.oabpe.domain.instituicao;
+
+import jakarta.validation.ConstraintViolation;
+
+import jakarta.validation.Validator;
+import lombok.RequiredArgsConstructor;
+import org.portodigital.residencia.oabpe.domain.commons.ImportProcessor;
+import org.portodigital.residencia.oabpe.domain.identidade.model.User;
+import org.portodigital.residencia.oabpe.domain.instituicao.dto.InstituicaoRequestDTO;
+import org.portodigital.residencia.oabpe.domain.prestacao_contas_subseccional.subseccional.Subseccional;
+import org.portodigital.residencia.oabpe.domain.prestacao_contas_subseccional.subseccional.dto.SubseccionalRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+public class InstituicaoImportProcessor implements ImportProcessor<InstituicaoRequestDTO> {
+
+    private final Validator validator;
+
+    @Override
+    public String[] getRequiredHeaders() {
+        return new String[]{"Instituição"};
+    }
+
+    @Override
+    public InstituicaoRequestDTO parse(Map<String, String> rowData) {
+        InstituicaoRequestDTO dto = new InstituicaoRequestDTO();
+        dto.setNome(rowData.get("Instituição"));
+        return dto;
+    }
+
+    @Override
+    public void validate(InstituicaoRequestDTO dto) {
+        Set<ConstraintViolation<InstituicaoRequestDTO>> violations = validator.validate(dto);
+        if (!violations.isEmpty()) {
+            StringBuilder sb = new StringBuilder();
+            for (ConstraintViolation<?> violation : violations) {
+                sb.append(violation.getMessage()).append("; ");
+            }
+            throw new IllegalArgumentException("Validação falhou: " + sb);
+        }
+    }
+
+    @Override
+    public Object convertToEntity(InstituicaoRequestDTO dto, User user) {
+        Instituicao entity = new Instituicao();
+        entity.setDescricao(dto.getNome());
+        entity.setUser(user);
+        return entity;
+    }
+}
