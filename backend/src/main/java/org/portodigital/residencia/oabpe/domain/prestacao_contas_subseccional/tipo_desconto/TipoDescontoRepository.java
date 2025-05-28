250a7521b94db2b96648f0e977b42e93580b40ab
Cauã Jordão <153930450+cauaojordao@users.noreply.github.com>
2025-05-28 03:00:40 -0300

    Revert "Feature/implementacao auditoria de acoes"

--- backend/src/main/java/org/portodigital/residencia/oabpe/domain/prestacao_contas_subseccional/tipo_desconto/TipoDescontoRepository.java
+++
@@ -1,9 +0,0 @@
-package org.portodigital.residencia.oabpe.domain.prestacao_contas_subseccional.tipo_desconto;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TipoDescontoRepository extends JpaRepository<TipoDesconto, Long> {
-    Page<TipoDesconto> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
-}
--- backend/src/main/java/org/portodigital/residencia/oabpe/exception/UsernameAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package org.portodigital.residencia.oabpe.exception;
-
-public class UsernameAlreadyExistsException extends RuntimeException{
-
-    public UsernameAlreadyExistsException(String message){
-        super(message);
-    }
-}
